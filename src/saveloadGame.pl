9dbac5b15315d925001442c906ea9091a115349b
EdwTorch <edwardtl18k@gmail.com>
2026-05-13 21:53:47 +0700

    Fix and Feat : Logic Tambah Ekstensi .txt

--- src/saveloadGame.pl
+++ src/saveloadGame.pl
@@ -7,4 +7,11 @@ print_kartu_sisa([HeadUrutan|TailUrutan],LoadGameFormat,SaveGameFormat):-
     nl(LoadGameFormat),
     print_kartu_sisa(TailUrutan,LoadGameFormat,SaveGameFormat).
 
+insert_txt(Nama,Hasil):-
+    name(Nama,ListAscii),
+    insert_tail(ListAscii,46,ListAscii1), % tambah .
+    insert_tail(ListAscii1,116,ListAscii2), % tambah t
+    insert_tail(ListAscii2,120,ListAscii3),
+    insert_tail(ListAscii3,116,ListHasil),
+    name(Hasil,ListHasil).
 

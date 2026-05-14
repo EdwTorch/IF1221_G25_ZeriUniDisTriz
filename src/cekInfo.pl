de0ba82ff518106f3a6b124d7a16edc003e3eec5
EdwTorch <edwardtl18k@gmail.com>
2026-05-14 16:35:37 +0700

    Fix : Fix Compound Fact dan Fungsi Panjang

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -14,6 +14,6 @@ print_list_pemain([Nama|SisaDaftar]) :-
 print_info_pemain([]).
 print_info_pemain([NamaPemain|SisaPemain]) :-
     kartu_tangan(NamaPemain, ListKartuTangan),
-    panjang(0,ListKartuTangan, JmlKartu),
+    panjang(0, JmlKartu,ListKartuTangan),
     write('- '), write(NamaPemain), write(': '), write(JmlKartu), write(' kartu'), nl,
     print_info_pemain(SisaPemain).
--- src/startGame_ambilKartu.pl
+++ src/startGame_ambilKartu.pl
@@ -29,13 +29,14 @@ Jml>0,
 inputOrgMsg(MsgIn),
 inputOrgUlg(MsgUlg),
 NewIdx is Idx +1,
-(State =:=0->format('~w ~d: ',[MsgIn,NewIdx]);format('~w ',[MsgUlg])),
+(State =:=0-> format('~w ~d: ',[MsgIn,NewIdx]);format('~w ',[MsgUlg])),
 read(InputNama),
-(atomic(InputNama) -> ((searchPemain(Daftarskrg,InputNama)) ->
+(atomic(InputNama), \+ (integer(InputNama)) -> ((searchPemain(Daftarskrg,InputNama)) ->
     NewIdx is Idx +1, JmlSisa is Jml -1, insert_head(InputNama,Daftarskrg,Daftarbaru),
     helperinputPemain(NewIdx,JmlSisa,Daftarbaru,DaftarPemain,0);
     helperinputPemain(Idx,Jml,Daftarskrg,DaftarPemain,1)); 
-    format('Input harus berupa tipe atomic dengan kata yang diapit '' '),
+    write('Input harus berupa tipe atomic dengan kata yang diapit \'\' '),
+    nl,
     helperinputPemain(Idx,Jml,Daftarskrg,DaftarPemain,1)).
 
 
@@ -49,57 +50,57 @@ kocokurutan(UrutanPemain,Jml,List,New):- random_select(UrutanPemain,1,NextUrutan
 format('~w - ',[Dipilih]), insert_tail(List, Dipilih,Listbaru), kocokurutan(NextUrutan,NewJml, Listbaru,New).
 
 % fakta kartu (List Kartu)
-kartu('hijau','0',_).
-kartu('hijau','1',_).
-kartu('hijau','2',_).
-kartu('hijau','3',_).
-kartu('hijau','4',_).
-kartu('hijau','5',_).
-kartu('hijau','6',_).
-kartu('hijau','7',_).
-kartu('hijau','8',_).
-kartu('hijau','9',_).
+kartu('hijau',0,_).
+kartu('hijau',1,_).
+kartu('hijau',2,_).
+kartu('hijau',3,_).
+kartu('hijau',4,_).
+kartu('hijau',5,_).
+kartu('hijau',6,_).
+kartu('hijau',7,_).
+kartu('hijau',8,_).
+kartu('hijau',9,_).
 kartu('hijau','reverse',_).
 kartu('hijau','skip',_).
 kartu('hijau','plus_dua',_).
-kartu('merah','0',_).
-kartu('merah','1',_).
-kartu('merah','2',_).
-kartu('merah','3',_).
-kartu('merah','4',_).
-kartu('merah','5',_).
-kartu('merah','6',_).
-kartu('merah','7',_).
-kartu('merah','8',_).
-kartu('merah','9',_).
+kartu('merah',0,_).
+kartu('merah',1,_).
+kartu('merah',2,_).
+kartu('merah',3,_).
+kartu('merah',4,_).
+kartu('merah',5,_).
+kartu('merah',6,_).
+kartu('merah',7,_).
+kartu('merah',8,_).
+kartu('merah',9,_).
 kartu('merah','reverse',_).
 kartu('merah','skip',_).
 kartu('merah','plus_dua',_).
 
-kartu('biru','0',_).
-kartu('biru','1',_).
-kartu('biru','2',_).
-kartu('biru','3',_).
-kartu('biru','4',_).
-kartu('biru','5',_).
-kartu('biru','6',_).
-kartu('biru','7',_).
-kartu('biru','8',_).
-kartu('biru','9',_).
+kartu('biru',0,_).
+kartu('biru',1,_).
+kartu('biru',2,_).
+kartu('biru',3,_).
+kartu('biru',4,_).
+kartu('biru',5,_).
+kartu('biru',6,_).
+kartu('biru',7,_).
+kartu('biru',8,_).
+kartu('biru',9,_).
 kartu('biru','reverse',_).
 kartu('biru','skip',_).
 kartu('biru','plus_dua',_).
 
-kartu('kuning','0',_).
-kartu('kuning','1',_).
-kartu('kuning','2',_).
-kartu('kuning','3',_).
-kartu('kuning','4',_).
-kartu('kuning','5',_).
-kartu('kuning','6',_).
-kartu('kuning','7',_).
-kartu('kuning','8',_).
-kartu('kuning','9',_).
+kartu('kuning',0,_).
+kartu('kuning',1,_).
+kartu('kuning',2,_).
+kartu('kuning',3,_).
+kartu('kuning',4,_).
+kartu('kuning',5,_).
+kartu('kuning',6,_).
+kartu('kuning',7,_).
+kartu('kuning',8,_).
+kartu('kuning',9,_).
 kartu('kuning','reverse',_).
 kartu('kuning','skip',_).
 kartu('kuning','plus_dua',_).
@@ -108,8 +109,8 @@ kartu('hitam','wildcard',_).
 kartu('hitam','plus_empat',_).
 warna_kartu_discardpile(['merah','kuning','hijau','biru']).
 warna_kartu_ambilkartu(['merah','kuning','hijau','biru','hitam']).
-jenis_kartu_discardpile(['0','1','2','3','4','5','6','7','8','9']).
-jenis_kartu_ambilkartu_bukanwild(['0','1','2','3','4','5','6','7','8','9','plus_dua','reverse','skip']).
+jenis_kartu_discardpile([0,1,2,3,4,5,6,7,8,9]).
+jenis_kartu_ambilkartu_bukanwild([0,1,2,3,4,5,6,7,8,9,'plus_dua','reverse','skip']).
 jenis_kartu_wild(['plus_empat','wildcard']).
 
 random_discardpile(Element):- 

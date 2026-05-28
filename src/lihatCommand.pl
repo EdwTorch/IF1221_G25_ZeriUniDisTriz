a4f61cc3a2086f867435b59075b9058e6a84542d
there514 <theresiaestelina@gmail.com>
2026-05-28 20:03:44 +0700

    fix: validasi kartu +2, +4, dan wildcard berturut2 di lihatCommand

--- src/lihatCommand.pl
+++ src/lihatCommand.pl
@@ -1,5 +1,10 @@
 % Mengecek apakah pemain bisa mainkanKartu 
 valid_play([kartu(W,_,_)|_], WarnaNow, _) :- W == WarnaNow, !.
-valid_play([kartu(_,J,_)|_], _, JenisNow) :- J == JenisNow, !.
-valid_play([kartu('hitam',_,_)|_], _, _) :- !.
+valid_play([kartu(_,J,_)|_], _, JenisNow) :- 
+    J == JenisNow, 
+    J \== 'plus_dua', 
+    J \== 'plus_empat', 
+    J \== 'wildcard', !.
+valid_play([kartu('hitam',J,_)|_], _, JenisNow) :- 
+    J \== JenisNow, !.
 valid_play([_|Tail], WarnaNow, JenisNow) :- valid_play(Tail, WarnaNow, JenisNow).

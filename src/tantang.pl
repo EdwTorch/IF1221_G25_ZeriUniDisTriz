06637d2f26d97fd1cce03958c307107e985a09d1
there514 <theresiaestelina@gmail.com>
2026-05-30 21:40:43 +0700

    fix: bug plus 2 berturut2 di tantang

--- src/tantang.pl
+++ src/tantang.pl
@@ -1,4 +1,4 @@
 % cek punya yg cocok gak si tersangka
-cek_kecocokkan([kartu(Warna, _, _)|_], WarnaMeja, _) :- Warna \== hitam, Warna == WarnaMeja, !.
-cek_kecocokkan([kartu(_, Jenis, _)|_], _, JenisMeja) :- Jenis \== wildcard, Jenis \== plus_empat, Jenis == JenisMeja, !.
+cek_kecocokkan([kartu(Warna, _, _)|_], WarnaMeja, _) :- Warna \== 'hitam', Warna == WarnaMeja, !.
+cek_kecocokkan([kartu(_, Jenis, _)|_], _, JenisMeja) :- Jenis == JenisMeja, Jenis \== 'wildcard', Jenis \== 'plus_empat', Jenis \== 'plus_dua', !.
 cek_kecocokkan([_|Sisa], WarnaMeja, JenisMeja) :- cek_kecocokkan(Sisa, WarnaMeja, JenisMeja).
